69ac95a14ee5f4e1bafdec97ba3af428f363a771
cchase20 <cchase@chasemg.com>
2015-02-02 11:50:39 -0700

    Commit 1
    using namespace std

---
+++ gitHub-Testing.cpp
@@ -0,0 +1,14 @@
+// gitHub_Practice.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World" << endl;
+	cin.ignore();
+	return 0;
+}
